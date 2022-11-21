fe923d8211b7379ef3e521252b9aeaf5c0290ebc
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-11-21 22:50:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+     int i, N=0, primo=1, secondo=1, terzo;
+    cin>>N;
+    if (N>=2){
+
+    for(i=0;i<N;i++){
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+        cout<<terzo<<endl;
+    }}
+    else{
+        cout<<"errore"<<endl;
+    }
    return 0;
 }
